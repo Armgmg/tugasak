fe6be28da3a0bf8e4ed43677ec9715ebeb733b60
Armgmg <saputraarmando015@gmail.com>
2026-01-14 14:27:56 +0700

    Fix NotificationController index method - remove unclosed docblock

--- app/Http/Controllers/User/NotificationController.php
+++ app/Http/Controllers/User/NotificationController.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Auth;
 
 class NotificationController extends Controller
 {
-    /**
     public function index()
     {
         return view('user.notifications.index');
